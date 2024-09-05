cd86d0ee1bc092b209a7812916b58dd8d5559403
potatopoisoning <ydhn97@naver.com>
2024-09-06 08:46:49 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String s = "         ,r'\"7\n";
+		s += "r`-_   ,'  ,/\n";
+		s += " \\. \". L_r'\n";
+		s += "   `~\\/\n";
+		s += "      |\n";
+		s += "      |\n";
+		
+		System.out.println(s);
+	}
+}
